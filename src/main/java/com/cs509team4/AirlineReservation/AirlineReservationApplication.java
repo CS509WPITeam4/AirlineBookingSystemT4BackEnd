fed1e26f579bb9c6d889245550c341e33a2ee7bc
lokesh234 <lgangaramaney@wpi.edu>
2025-02-24 11:29:02 -0500

    Started Project

---
+++ src/main/java/com/cs509team4/AirlineReservation/AirlineReservationApplication.java
@@ -0,0 +1,13 @@
+package com.cs509team4.AirlineReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cs509team4/AirlineReservation/AirlineReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs509team4.AirlineReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
